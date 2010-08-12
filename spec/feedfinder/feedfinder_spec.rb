32a0e874b176a8577640a99623ef6e84c792c501
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2010-08-13 01:24:49 +0200

    Syntax cleanup

--- spec/feedfinder/feedfinder_spec.rb
+++ spec/feedfinder/feedfinder_spec.rb
@@ -2,18 +2,18 @@ require File.dirname(__FILE__) + "/../../lib/feedfinder"
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe "Feedfinder" do
-  
+
   before do
     @feeds = [
-      ['http://bjorkoy.com', 'http://feeds.feedburner.com/bjorkoy'],
-      ['http://w3.org', 'http://w3.org/News/atom.xml'],
-      ['http://newyorktimes.com', 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'],
-      ['http://googleblog.blogspot.com', 
+      ['http://bjorkoy.com', ['http://feeds.feedburner.com/bjorkoy']],
+      ['http://w3.org', ['http://w3.org/News/atom.xml']],
+      ['http://newyorktimes.com', ['http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml']],
+      ['http://googleblog.blogspot.com',
         ['http://googleblog.blogspot.com/feeds/posts/default',
          'http://googleblog.blogspot.com/feeds/posts/default?alt=rss']]
     ]
   end
-  
+
   describe ".feeds" do
     it "Should return an array of feeds" do
       @feeds.each do |urls|
