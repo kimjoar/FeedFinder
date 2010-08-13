4f92afc8dc7cc9eed4daa24d8ad953c056355bc2
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2010-08-13 02:29:46 +0200

    Using FakeWeb to speed up tests A LOT!

--- spec/feedfinder/feedfinder_spec.rb
+++ spec/feedfinder/feedfinder_spec.rb
@@ -2,32 +2,20 @@ require File.dirname(__FILE__) + "/../../lib/feedfinder"
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe "Feedfinder" do
-
-  before do
-    @feeds = [
-      ['http://bjorkoy.com', ['http://feeds.feedburner.com/bjorkoy']],
-      ['http://w3.org', ['http://w3.org/News/atom.xml']],
-      ['http://newyorktimes.com', ['http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml']],
-      ['http://googleblog.blogspot.com',
-        ['http://googleblog.blogspot.com/feeds/posts/default',
-         'http://googleblog.blogspot.com/feeds/posts/default?alt=rss']]
-    ]
-  end
-
   describe ".feeds" do
     it "Should return an array of feeds" do
-      @feeds.each do |urls|
+      feeds.each do |urls|
         FeedFinder.feeds(urls[0]).should == urls[1].to_a
       end
     end
 
     it "should yield if a block is given and there are more than one feeds" do
-      FeedFinder.feeds(@feeds.last[0]) { |feeds| feeds.length.should > 1 }
+      FeedFinder.feeds(several_feeds[0]) { |feeds| feeds.length.should > 1 }
     end
 
     it "should not yield if a block is given and there are more than one feeds" do
       yielded = false
-      FeedFinder.feeds(@feeds.first[0]) { |feeds| yielded = true }
+      FeedFinder.feeds(single_feed[0]) { |feeds| yielded = true }
       yielded.should be_false
     end
 
---
+++ spec/spec_helper.rb
@@ -0,0 +1,60 @@
+require 'fakeweb'
+
+def single_feed
+  feeds.first
+end
+
+def several_feeds
+  feeds.last
+end
+
+def feeds
+  [
+    ['http://bjorkoy.com', ['http://feeds.feedburner.com/bjorkoy']],
+    ['http://w3.org', ['http://w3.org/News/atom.xml']],
+    ['http://newyorktimes.com', ['http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml']],
+    ['http://googleblog.blogspot.com',
+      ['http://googleblog.blogspot.com/feeds/posts/default',
+       'http://googleblog.blogspot.com/feeds/posts/default?alt=rss']
+    ]
+  ]
+end
+
+class String
+  def slugify
+    str = self.dup
+    str.gsub!(/[^a-zA-Z0-9 ]/,"")
+    str.gsub!(/[ ]+/," ")
+    str.gsub!(/ /,"-")
+    str.downcase!
+    str
+  end
+end
+
+def cache_dir
+  File.join(File.dirname(__FILE__), 'cache')
+end
+
+def cache!
+  Dir.mkdir(cache_dir) unless File.directory?(cache_dir)
+  feeds.flatten.each { |f| cache_url! f }
+end
+
+def cache_url!(url)
+  File.open(File.join(cache_dir, url.slugify), 'w') do |f|
+    f.write open(url).read
+  end
+end
+
+def cached?
+  Dir.entries(cache_dir).length > 0 rescue false
+end
+
+def fake_it!
+  feeds.flatten.each do |f|
+    FakeWeb.register_uri :get, f, :body => File.read(File.join(cache_dir, f.slugify))
+  end
+end
+
+cache! unless cached?
+fake_it!
